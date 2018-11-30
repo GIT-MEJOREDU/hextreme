b8853b8fc624385d9cea8fb142a2f9a0dae63828
Cristina Romero R <cristinaromerorojas@gmail.com>
2018-11-30 15:46:55 -0500

    Se crea metodo para insertr letras descubiertas

--- lib/diccionario.rb
+++ lib/diccionario.rb
@@ -1,6 +1,13 @@
 class Diccionario
 
+    attr_accessor :historico
+
     palabras = {1 => 'sprint', 2 => "review", 3 => "backlog", 4 => "planning", 5 => "meeting"}
+
+    def initialize palabra = "sprint"
+        total_letras = palabra.size
+        @historico = ("_" * total_letras)
+    end
 
     def dame_palabra
         "sprint"
@@ -17,4 +24,13 @@ class Diccionario
        end
        return posiciones
     end
+
+    def aproximacion p_palabra, p_letra
+        posiciones = posiciones(p_palabra, p_letra)
+        posiciones.each do |posicion|
+            @historico[posicion] = p_letra
+        end
+    end
+
+
 end
--- spec/code_diccionario_spec.rb
+++ spec/code_diccionario_spec.rb
@@ -15,4 +15,21 @@ describe Diccionario do
         resultado = dicc.posiciones("sprinti", "i")
         expect(resultado).to eq [3, 6]
     end
+
+    it "debe de regresar la letra en la posicion que exista" do
+        dicc = Diccionario.new
+        dicc.aproximacion("sprinti", "i")
+        resultado = dicc.historico.split("").join(" ")
+        expect(resultado).to eq "_ _ _ i _ _ i"
+    end
+
+    it "debe de regresar las letras en la posicion que existan" do
+        dicc = Diccionario.new
+        #dicc.aprox = "_ _ _ i _ _"
+        dicc.aproximacion("sprint", "i")
+        dicc.aproximacion("sprint", "r")
+        resultado = dicc.historico.split("").join(" ")
+        expect(resultado).to eq "_ _ r i _ _"
+    end
+
 end
