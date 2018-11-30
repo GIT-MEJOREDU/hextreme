8462ed7cf3c3ed2da0bdab01c9448269a1fbf72c
Cristina Romero R <cristinaromerorojas@gmail.com>
2018-11-30 13:08:12 -0500

    Se agrega busqueda de letra en palabra

--- spec/code_diccionario_spec.rb
+++ spec/code_diccionario_spec.rb
@@ -5,4 +5,14 @@ describe Diccionario do
         resultado = dicc.dame_palabra
         expect(resultado).to eq "sprint"
     end
+    it "debe regresar posiciones en palabra dada una letra" do
+        dicc = Diccionario.new
+        resultado = dicc.posiciones("sprint", "i")
+        expect(resultado).to eq [3]
+    end
+    it "debe regresar posiciones en palabra dada una letra" do
+        dicc = Diccionario.new
+        resultado = dicc.posiciones("sprinti", "i")
+        expect(resultado).to eq [3, 6]
+    end
 end
